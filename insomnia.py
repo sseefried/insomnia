cc733cafa481a879b831a59d9e8406e6a73a8ddd
Sean Seefried <sean@seefried.com.au>
2022-08-11 09:49:20 +1000

    Change messages

--- insomnia.py
+++ insomnia.py
@@ -4,11 +4,13 @@ import subprocess
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 try:
+    msg="Turning off insomnia. Computer will now go to sleep as normal"
     pid = subprocess.check_output(["pgrep", "-f", "caf.py"]).decode("utf-8").strip()
     subprocess.Popen(["kill", pid])
-    subprocess.Popen(["notify-send", "Computer doesn't stay awake..."])
-    print("Computer doesn't stay awake...")
+    subprocess.Popen(["notify-send", msg])
+    print(msg)
 except:
+    msg="Computer will now stay awake..."
     subprocess.Popen(["/bin/bash", "-c", script_dir+"/"+"caf.py"])
-    subprocess.Popen(["notify-send", "Computer stays awake..."])
-    print("Computer stays awake...")
+    subprocess.Popen(["notify-send", msg])
+    print(msg)
